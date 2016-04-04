063d25864594e6345799e763fff754993e8f0c04
Misirian <misiriaj@ad.matc.edu>
2016-04-04 10:03:32 -0500

    Added FarmerGUI

---
+++ FarmerGUI/FarmerGUI/frmMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace FarmerGUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbChicken = new System.Windows.Forms.CheckBox();
+            this.cbGrain = new System.Windows.Forms.CheckBox();
+            this.cbFox = new System.Windows.Forms.CheckBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbChicken
+            // 
+            this.cbChicken.AutoSize = true;
+            this.cbChicken.Location = new System.Drawing.Point(323, 82);
+            this.cbChicken.Name = "cbChicken";
+            this.cbChicken.Size = new System.Drawing.Size(65, 17);
+            this.cbChicken.TabIndex = 0;
+            this.cbChicken.Text = "Chicken";
+            this.cbChicken.UseVisualStyleBackColor = true;
+            // 
+            // cbGrain
+            // 
+            this.cbGrain.AutoSize = true;
+            this.cbGrain.Location = new System.Drawing.Point(217, 82);
+            this.cbGrain.Name = "cbGrain";
+            this.cbGrain.Size = new System.Drawing.Size(51, 17);
+            this.cbGrain.TabIndex = 1;
+            this.cbGrain.Text = "Grain";
+            this.cbGrain.UseVisualStyleBackColor = true;
+            // 
+            // cbFox
+            // 
+            this.cbFox.AutoSize = true;
+            this.cbFox.Location = new System.Drawing.Point(469, 82);
+            this.cbFox.Name = "cbFox";
+            this.cbFox.Size = new System.Drawing.Size(43, 17);
+            this.cbFox.TabIndex = 2;
+            this.cbFox.Text = "Fox";
+            this.cbFox.UseVisualStyleBackColor = true;
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(54, 78);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(93, 23);
+            this.btnMove.TabIndex = 3;
+            this.btnMove.Text = "Move Farmer!";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(272, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Water";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 420);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.cbFox);
+            this.Controls.Add(this.cbGrain);
+            this.Controls.Add(this.cbChicken);
+            this.Name = "frmMain";
+            this.Text = "FamerGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbChicken;
+        private System.Windows.Forms.CheckBox cbGrain;
+        private System.Windows.Forms.CheckBox cbFox;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FarmerGUI/FarmerGUI/frmMain.cs
@@ -0,0 +1,120 @@
+﻿// Written by Jacob Misirian
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FarmerGUI
+{
+    public partial class frmMain : Form
+    {
+        private FarmerGame game;
+        private const int MOVE_OFFSET = 100;
+        public frmMain()
+        {
+            InitializeComponent();
+            game = new FarmerGame();
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if (calculateTotalChecked() > 1)
+            {
+                MessageBox.Show("Error! Can only have one item checked!");
+                return;
+            }
+            CheckBox checkedBox = getCheckedControl();
+            Item item = (checkedBox == null) ? Item.Farmer : FarmerGame.ToItem(checkedBox.Text);
+
+            if (game.NorthBank.Contains(item))
+                game.Move(Direction.South, item);
+            else if (game.SouthBank.Contains(item))
+                game.Move(Direction.North, item);
+
+            if (item != Item.Farmer)
+                moveItem(checkedBox);
+            moveFarmer();
+            determineOutcome();
+            clearChecks();
+        }
+
+        private void moveItem(CheckBox checkBox)
+        {
+            Item item = FarmerGame.ToItem(checkBox.Text);
+            if (game.NorthBank.Contains(item))
+                checkBox.Location = new Point(checkBox.Location.X, checkBox.Location.Y - MOVE_OFFSET);
+            else
+                checkBox.Location = new Point(checkBox.Location.X, checkBox.Location.Y + MOVE_OFFSET);
+        }
+
+        private void moveFarmer()
+        {
+            if (game.NorthBank.Contains(Item.Farmer))
+                btnMove.Location = new Point(btnMove.Location.X, btnMove.Location.Y - MOVE_OFFSET);
+            else
+                btnMove.Location = new Point(btnMove.Location.X, btnMove.Location.Y + MOVE_OFFSET);
+        }
+
+        private int calculateTotalChecked()
+        {
+            int total = 0;
+            if (cbChicken.Checked)
+                total++;
+            if (cbFox.Checked)
+                total++;
+            if (cbGrain.Checked)
+                total++;
+            return total;
+        }
+
+        private CheckBox getCheckedControl()
+        {
+            if (cbChicken.Checked)
+                return cbChicken;
+            if (cbFox.Checked)
+                return cbFox;
+            if (cbGrain.Checked)
+                return cbGrain;
+            return null;
+        }
+
+        private void determineOutcome()
+        {
+            if (game.DetermineWin())
+            {
+                MessageBox.Show("You won!");
+                reset();
+            }
+            if (game.DetermineLoss())
+            {
+                MessageBox.Show("You lost!");
+                reset();
+            }
+        }
+
+        private void reset()
+        {
+            if (game.SouthBank.Contains(Item.Chicken))
+                cbChicken.Location = new Point(cbChicken.Location.X, cbChicken.Location.Y - MOVE_OFFSET);
+            if (game.SouthBank.Contains(Item.Fox))
+                cbFox.Location = new Point(cbFox.Location.X, cbFox.Location.Y - MOVE_OFFSET);
+            if (game.SouthBank.Contains(Item.Grain))
+                cbGrain.Location = new Point(cbGrain.Location.X, cbGrain.Location.Y - MOVE_OFFSET);
+            if (game.SouthBank.Contains(Item.Farmer))
+                btnMove.Location = new Point(btnMove.Location.X, btnMove.Location.Y - MOVE_OFFSET);
+        }
+
+        private void clearChecks()
+        {
+            cbChicken.Checked = false;
+            cbFox.Checked = false;
+            cbGrain.Checked = false;
+        }
+    }
+}
